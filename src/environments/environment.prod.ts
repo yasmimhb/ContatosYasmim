3e902e8dd713982bd3790bdc91568826fe60bed3
Yasmim Haffermann Bilovus <128074694+yasmimhb@users.noreply.github.com>
2023-11-09 09:05:48 -0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+      apiKey: "AIzaSyBkJHFeHUwBXnxKX6tGeaO4e3qI2QS-iTw",
+      authDomain: "contatos2tb.firebaseapp.com",
+      projectId: "contatos2tb",
+      storageBucket: "contatos2tb.appspot.com",
+      messagingSenderId: "9844896696",
+      appId: "1:9844896696:web:8a27c0cef1c205baa4d54d",
+  }
+};
